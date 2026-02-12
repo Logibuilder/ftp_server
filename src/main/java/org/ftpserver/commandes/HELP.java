ac37bd4b97a7dc931431ddb39b068f5de6457640
Assane KANE <assane.kane2.etu.univ-lille.fr>
2026-02-12 17:55:45 +0100

    commande HELP OK

--- src/main/java/org/ftpserver/commandes/HELP.java
+++ src/main/java/org/ftpserver/commandes/HELP.java
@@ -7,14 +7,35 @@ import java.io.IOException;
 
 /**
  * Implémentation de la commande HELP.
- * <p>
  * Fournit au client des informations sur les commandes supportées par le serveur.
- * Si aucun argument n'est fourni, liste l'ensemble des fonctionnalités disponibles.
- * </p>
  */
 public class HELP implements FTPCommande {
     @Override
     public void execute(String[] args, Client client) throws IOException {
+        if (args.length == 0) {
+            // Liste globale des commandes supportées
+            String supportedCommands = "USER PASS PWD CWD CDUP LIST NLST RETR STOR MKD DELE RMD RNFR RNTO SIZE MDTM TYPE SYST QUIT HELP PASV";
+            client.getControllerSocket().write("214-Les commandes suivantes sont reconnues :");
+            client.getControllerSocket().write("    " + supportedCommands);
+            client.getControllerSocket().write("214 Fin de l'aide.");
+        } else {
+            // Aide spécifique pour une commande donnée
+            String cmdToHelp = args[0].toUpperCase();
+            String helpMessage;
 
+            switch (cmdToHelp) {
+                case "USER": helpMessage = "214 Syntaxe: USER <nom_utilisateur>"; break;
+                case "PASS": helpMessage = "214 Syntaxe: PASS <mot_de_passe>"; break;
+                case "CWD":  helpMessage = "214 Syntaxe: CWD <chemin_repertoire>"; break;
+                case "RETR": helpMessage = "214 Syntaxe: RETR <nom_fichier> (Télécharger)"; break;
+                case "STOR": helpMessage = "214 Syntaxe: STOR <nom_fichier> (Téléverser)"; break;
+                case "PASV": helpMessage = "214 Entre en mode passif pour le transfert de données."; break;
+                case "QUIT": helpMessage = "214 Ferme la session FTP."; break;
+                default:
+                    helpMessage = "214 Commande inconnue. Tapez HELP pour la liste.";
+                    break;
+            }
+            client.getControllerSocket().write(helpMessage);
+        }
     }
 }
